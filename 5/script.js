ced29ce18c2dc5ab9a6f8d30014c65e222a7ce5c
Daniil-Ketov <90641989+Daniil-Ketov@users.noreply.github.com>
2021-10-21 19:10:25 +0300

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -1,5 +1,9 @@
-window.addEventListener('DOMContentLoaded', function (event) {
-    console.log("DOM fully loaded and parsed");
+window.addEventListener("DOMContentLoaded", function () {
+    let b = document.getElementById("calcButton");
+    b.addEventListener("click", function (event) {
+        b = event.target;
+        calculate();
+    });
 });
 
 function calculate() {
@@ -8,16 +12,14 @@ function calculate() {
 
     if (price[0].value < 0 || count[0].value < 0) {
         alert("Вводите целые положительные числа");
-    }
-    else if (price[0].value.match(/[.,]/) != null || count[0].value.match(/[.,]/) != null) {
+    } else if (price[0].value.match(/[.,]/) !== null || count[0].value.match(/[.,]/) !== null) {
         alert("Вводите целые числа");
-    }
-    else if (price[0].value.match(/[^0-9]/) != null || count[0].value.match(/[^0-9]/) != null) {
+    } else if (price[0].value.match(/[^0-9]/) !== null || count[0].value.match(/[^0-9]/) !== null) {
         alert("Вводите числа");
-    }
-    else {
+    } else {
         const result = Number(price[0].value) * Number(count[0].value);
-        document.getElementById("result").innerHTML = "Стоимость покупки: " + result + " денежных единиц";
+        const out = "Стоимость покупки: " + result + " денежных единиц";
+        document.getElementById("result").innerHTML = out;
     }
     return false;
 }
